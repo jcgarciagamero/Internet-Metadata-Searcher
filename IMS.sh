73fc7ce529a0802b175a2b8e70210170953e9fe4
José C. García Gamero <jcgarciagamero@gmail.com>
2017-03-15 20:14:45 +0100

    Add files via upload

---
+++ IMS.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#Lince Metadata 1.0
+#Author: José C. García Gamero.
+#Date: 2017-03-15
+#E-Mail: josecarlos@whatinformatica.com
+
+sudo chmod +x ./options.sh
+
+numero=$(zenity --width=480 --height=320 --entry --entry-text="1,2,3..." --text="Seleccione el número de resultados que quiere obtener")
+	var1=$(echo $?)
+
+	if [ $var1 = 1 ]; then
+
+		exit
+
+	else url=$(zenity --width=480 --height=320 --entry --entry-text="site.com" --text="Introduzca la URL")
+		var2=$(echo $?)
+
+		 if [ $var2 = 1 ]; then
+
+		 exit
+
+		 	else tipo=$(zenity --width=480 --height=320 --entry --entry-text="pdf jpg doc" --text="Introduzca el tipo de fichero")
+			var3=$(echo $?)
+
+				if [ $var3 = 1 ]; then
+
+		 		exit
+
+		 			else #Búsqueda de resultados en google
+					googler --np -n"$numero" site:"$url" filetype:"$tipo" > archivo.txt
+
+					#Filtro uno
+					grep -i "http://" archivo.txt > archivo1.txt
+
+					#Filtro dos
+					grep -i ".$tipo" archivo1.txt > fichero.txt
+
+					#filtro tres
+					cut -d "m" -f 2,3,4,5,6,7,8,9,10,12,13,14,15 fichero.txt > fichero2.txt
+
+					#filtro cuatro
+					cut -d "[" -f 1 fichero2.txt > fichero3.txt
+
+					#filtro cinco
+					sed -e 's/.$//' fichero3.txt > fichero4.txt
+
+					#Direcciones de ficheros
+					nombre=$(cat fichero4.txt)
+
+					#Descarga de ficheros
+					wget $nombre -P analist
+
+					rm -r archivo.txt archivo1.txt fichero.txt fichero2.txt fichero3.txt fichero4.txt
+
+					zenity --info \
+					--text="Estrayendo metadatos"
+
+					exiftool analist/* > metadatos.txt
+
+					./options.sh
+
+				
+		fi
+	fi
+fi
+exit 
+
---
+++ options.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#Lince Metadata 1.0
+#Author: José C. García Gamero.
+#Date: 2017-03-15
+#E-Mail: josecarlos@whatinformatica.com
+
+eleccion=$(zenity --width=480 --height=320 --list --title "Metadatos extraidos con éxito" --column Descripción "Todos los metadatos" "Usuarios" "Software" "Directorios")
+					respuesta=$(echo $?)
+
+						if [ $respuesta = 1 ]; then
+
+							exit
+
+						elif [ "$eleccion" = "Todos los metadatos" ]; then
+						zenity --title="Todos los metadatos encontrados" --text-info --filename="metadatos.txt"
+
+						elif [ "$eleccion" = "Usuarios" ]; then
+						grep -i "Author" metadatos.txt > usuarios.txt
+						zenity --title="Usuarios encontrados" --text-info --filename="usuarios.txt"
+
+						elif [ "$eleccion" = "Software" ]; then
+						grep -i "Producer" metadatos.txt > software.txt
+						zenity --title="Software" --text-info --filename="software.txt"
+
+						elif [ "$eleccion" = "Directorios" ]; then
+						grep -i "Manifest Reference File Path" metadatos.txt > directorios.txt 
+						zenity --title="Directorios" --text-info --filename="directorios.txt"
+
+						fi
+				
+				zenity --question \
+				--text="¿Desde realizar algo más?"
+
+				if [ $respuesta = 1 ]; then
+
+				exit
+				else 
+
+					eleccion=$(zenity --width=480 --height=320 --list --title "Escoja una opción" --column Descripción "Nueva busqueda" "Todos los metadatos" "Usuarios" "Software" "Directorios")
+					respuesta=$(echo $?)
+
+						if [ $respuesta = 1 ]; then
+
+							exit
+
+						elif [ "$eleccion" = "Nueva busqueda" ]; then
+						./IMS.sh
+
+						elif [ "$eleccion" = "Todos los metadatos" ]; then
+						zenity --title="Todos los metadatos encontrados" --text-info --filename="metadatos.txt"
+
+						elif [ "$eleccion" = "Usuarios" ]; then
+						grep -i "Author" metadatos.txt > usuarios.txt
+						zenity --title="Usuarios encontrados" --text-info --filename="usuarios.txt"
+
+						elif [ "$eleccion" = "Software" ]; then
+						grep -i "Producer" metadatos.txt > software.txt
+						zenity --title="Software" --text-info --filename="software.txt"
+
+						elif [ "$eleccion" = "Directorios" ]; then
+						grep -i "Manifest Reference File Path" metadatos.txt > directorios.txt 
+						zenity --title="Directorios" --text-info --filename="directorios.txt"
+
+						fi
+
+				fi 
